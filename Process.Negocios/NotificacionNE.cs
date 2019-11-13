01ab170b1beccd8349fc53f42a24053e55c311ef
alrivasp <alvaro.rivas.p@gmail.com>
2019-11-12 21:07:41 -0300

    Desarrollo servicio de Notificacion

---
+++ Process.Negocios/NotificacionNE.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Process.Datos;
+using Process.Modelos;
+
+namespace Process.Negocios
+{
+    public class NotificacionNE
+    {
+        public int notificacionAsignacion(string _correo_destino, int _id_tarea, string _nombre_tarea, string _flujo_nombre, string _nombre_funcionario_destino, string _nombre_funcionario_origen, string _nombre_equipo, string _nombre_unidad, string _nombre_empresa)
+        {
+            //Instancia de libreria Mail
+            System.Net.Mail.MailMessage auxCorreo = new System.Net.Mail.MailMessage();
+
+            try
+            {
+                int retorno = 0;
+
+                //Asunto Correo
+                string _asunto_correo = "ATENCION Asignacion de Tarea, se le acaba de asignar una Tarea en Plataforma PROCESS";
+                //Cuerpo Correo
+                string _cuerpo_correo = "Estimado " + _nombre_funcionario_destino.ToUpper() + "\n\r" +
+                                        "Le notificamos que se le acaba de asignar la tarea Nº " + _id_tarea + " con el nombre:" + _nombre_tarea.ToUpper() + "\n\r" +
+                                        "que pertenece al Flujo de trabajo " + _flujo_nombre.ToUpper() + ", esta tarea fue asignada por " + _nombre_funcionario_origen.ToUpper() + " que pertenece al equipo de trabajo " + _nombre_equipo.ToUpper() + "\n\r" +
+                                        "correspondiente a la Unidad " + _nombre_unidad.ToUpper() + " de la Empresa " + _nombre_empresa.ToUpper() + " ." + "\n\r" +
+                                        "Favor estar atento a su tarea recien asignada." + "\n\r" +
+                                        "\n\r" +
+                                        "\n\r" +
+                                        "Saludos" + "\n\r" +
+                                        "Atentamente Plataforma PROCESS.";
+
+                //Creacion de formato correo
+                //Agregar Destinatario funcionario asignado
+                auxCorreo.To.Add(_correo_destino);                
+                //Agregar asunto
+                auxCorreo.Subject = _asunto_correo;
+                auxCorreo.SubjectEncoding = System.Text.Encoding.UTF8;
+                //Agregar Cuerpo de Mensaje
+                auxCorreo.Body = _cuerpo_correo;
+                auxCorreo.BodyEncoding = System.Text.Encoding.UTF8;
+                auxCorreo.IsBodyHtml = false;
+                //Agregar Remitente de Correo
+                auxCorreo.From = new System.Net.Mail.MailAddress("CONTACTO.PROCESS@gmail.com");
+                //Crear cliente correo
+                System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+                cliente.Credentials = new System.Net.NetworkCredential("CONTACTO.PROCESS@gmail.com", "Portafolio.2019");
+                cliente.Port = 587;
+                cliente.EnableSsl = true;
+                cliente.Host = "smtp.gmail.com";// mail.dominio.com
+                                                //Enviar correo
+                cliente.Send(auxCorreo);
+
+                return retorno;
+
+            }
+            catch (Exception)
+            {
+                return -1;
+
+            }
+
+        }
+
+        public int notificacionRechazo(string _correo_destino, int _id_tarea, string _nombre_tarea, string _flujo_nombre, string _nombre_funcionario_destino, string _nombre_funcionario_origen, string _nombre_equipo, string _nombre_unidad, string _nombre_empresa, string _motivo)
+        {
+            //Instancia de libreria Mail
+            System.Net.Mail.MailMessage auxCorreo = new System.Net.Mail.MailMessage();
+
+            try
+            {
+                int retorno = 0;
+
+                //Asunto Correo
+                string _asunto_correo = "ATENCION Rechazo de Tarea, se acaba de Rechazar una tarea asignada por Usted en Plataforma PROCESS";
+                //Cuerpo Correo
+                string _cuerpo_correo = "Estimado " + _nombre_funcionario_destino.ToUpper() + "\n\r" +
+                                        "Le notificamos que se acaba de rechazar la tarea Nº " + _id_tarea + " con el nombre:" + _nombre_tarea.ToUpper() + "\n\r" +
+                                        "que pertenece al Flujo de trabajo " + _flujo_nombre.ToUpper() + ", esta tarea fue rechazada por " + _nombre_funcionario_origen.ToUpper() + " que pertenece al equipo de trabajo " + _nombre_equipo.ToUpper() + "\n\r" +
+                                        "correspondiente a la Unidad " + _nombre_unidad.ToUpper() + " de la Empresa " + _nombre_empresa.ToUpper() + " ." + "\n\r" +
+                                        "El motivo del rechazo es el siguiente " + _motivo.ToUpper() + "." + "\n\r" +
+                                        "\n\r" +
+                                        "\n\r" +
+                                        "Saludos" + "\n\r" +
+                                        "Atentamente Plataforma PROCESS.";
+
+                //Creacion de formato correo
+                //Agregar Destinatario funcionario asignado
+                auxCorreo.To.Add(_correo_destino);
+                //Agregar asunto
+                auxCorreo.Subject = _asunto_correo;
+                auxCorreo.SubjectEncoding = System.Text.Encoding.UTF8;
+                //Agregar Cuerpo de Mensaje
+                auxCorreo.Body = _cuerpo_correo;
+                auxCorreo.BodyEncoding = System.Text.Encoding.UTF8;
+                auxCorreo.IsBodyHtml = false;
+                //Agregar Remitente de Correo
+                auxCorreo.From = new System.Net.Mail.MailAddress("CONTACTO.PROCESS@gmail.com");
+                //Crear cliente correo
+                System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+                cliente.Credentials = new System.Net.NetworkCredential("CONTACTO.PROCESS@gmail.com", "Portafolio.2019");
+                cliente.Port = 587;
+                cliente.EnableSsl = true;
+                cliente.Host = "smtp.gmail.com";// mail.dominio.com
+                                                //Enviar correo
+                cliente.Send(auxCorreo);
+
+                return retorno;
+
+            }
+            catch (Exception)
+            {
+                return -1;
+
+            }
+
+        }
+    }
+}
---
+++ Process.Servicios/Process_Notificacion.asmx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Oracle.ManagedDataAccess.Client;
+using Process.Negocios;
+using Process.Modelos;
+using System.Web.Script.Services;
+using Newtonsoft.Json;
+using System.Dynamic;
+using System.Data;
+
+
+namespace Process.Servicios
+{
+    /// <summary>
+    /// Descripción breve de Process_Notificacion
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Process_Notificacion : System.Web.Services.WebService
+    {
+        private NotificacionNE notificacionNE = new NotificacionNE();
+
+        //////////////////////////////////////
+        ////Web Metodos para APP WEB
+        //////////////////////////////////////
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public void notificacionAsignacion_Web(string json)
+        {
+            try
+            {
+                CadenaConexion();
+                int retorno = 0;
+
+                dynamic dataJson = new ExpandoObject();
+                dynamic datosRespuesta = new ExpandoObject();
+
+                dataJson = JsonConvert.DeserializeObject<dynamic>(json);
+
+                string _correo_destino = dataJson.CORREO_DESTINO;
+                int _id_tarea = dataJson.ID_TAREA;
+                string _nombre_tarea = dataJson.NOMBRE_TAREA;
+                string _flujo_nombre = dataJson.FLUJO_NOMBRE;
+                string _nombre_funcionario_destino = dataJson.NOMBRE_FUNCIONARIO_DESTINO;
+                string _nombre_funcionario_origen = dataJson.NOMBRE_FUNCIONARIO_ORIGEN;                              
+                string _nombre_equipo = dataJson.NOMBRE_EQUIPO;
+                string _nombre_unidad = dataJson.NOMBRE_UNIDAD;
+                string _nombre_empresa = dataJson.NOMBRE_EMPRESA;
+
+
+                retorno = notificacionNE.notificacionAsignacion(_correo_destino, _id_tarea, _nombre_tarea, _flujo_nombre, _nombre_funcionario_destino, _nombre_funcionario_origen, _nombre_equipo, _nombre_unidad,_nombre_empresa);
+
+                datosRespuesta.datos = retorno;
+
+                string JSONString = string.Empty;
+                JSONString = JsonConvert.SerializeObject(datosRespuesta);
+                Context.Response.ContentType = "application/json";
+                Context.Response.Write(JSONString);
+
+            }
+            catch (Exception ex)
+            {
+                Context.Response.ContentType = "application/json";
+                Context.Response.Write("Error : " + ex.Message);
+            }
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public void notificacionRechazo_Web(string json)
+        {
+            try
+            {
+                CadenaConexion();
+                int retorno = 0;
+
+                dynamic dataJson = new ExpandoObject();
+                dynamic datosRespuesta = new ExpandoObject();
+
+                dataJson = JsonConvert.DeserializeObject<dynamic>(json);
+
+                string _correo_destino = dataJson.CORREO_DESTINO;
+                int _id_tarea = dataJson.ID_TAREA;
+                string _nombre_tarea = dataJson.NOMBRE_TAREA;
+                string _flujo_nombre = dataJson.FLUJO_NOMBRE;
+                string _nombre_funcionario_destino = dataJson.NOMBRE_FUNCIONARIO_DESTINO;
+                string _nombre_funcionario_origen = dataJson.NOMBRE_FUNCIONARIO_ORIGEN;
+                string _nombre_equipo = dataJson.NOMBRE_EQUIPO;
+                string _nombre_unidad = dataJson.NOMBRE_UNIDAD;
+                string _nombre_empresa = dataJson.NOMBRE_EMPRESA;
+                string _motivo = dataJson.MOTIVO;
+
+
+                retorno = notificacionNE.notificacionRechazo(_correo_destino, _id_tarea, _nombre_tarea, _flujo_nombre, _nombre_funcionario_destino, _nombre_funcionario_origen, _nombre_equipo, _nombre_unidad, _nombre_empresa, _motivo);
+
+                datosRespuesta.datos = retorno;
+
+                string JSONString = string.Empty;
+                JSONString = JsonConvert.SerializeObject(datosRespuesta);
+                Context.Response.ContentType = "application/json";
+                Context.Response.Write(JSONString);
+
+            }
+            catch (Exception ex)
+            {
+                Context.Response.ContentType = "application/json";
+                Context.Response.Write("Error : " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// CONEXION 
+        /// </summary>
+        public void CadenaConexion()// Método que extrae la conexión del webconfig y la pasa a una variable global para reutilizarla en la capa de datos
+        {
+            //Support.Support desEncrip = new Support.Support();// Esto es para la encriptación de la conexión
+            String cn = "";
+            cn = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Process"].ConnectionString;
+            OracleConnectionStringBuilder cnn = new OracleConnectionStringBuilder(cn);
+
+            string dbs = cnn.DataSource;
+            string usid = cnn.UserID;
+            //string psw = desEncrip.Deserialice(cnn.Password);
+            string psw = cnn.Password;
+            string conex = string.Empty;
+
+            conex = "Data Source=" + dbs + "; User ID=" + usid + "; Password=" + psw;
+
+            OracleConnection sqlString = new OracleConnection();
+            sqlString.ConnectionString = conex;
+
+            Global.CadenaConexionGlobal = sqlString;
+        }
+    }
+}
